bc5f8b72300fb614f6d640c0113a84efbbe76dbb
OleksiiMyzgin <oleksii.myzgin@dentsplysirona.com>
2020-03-25 10:35:40 +0200

    add Ripple button

---
+++ src/RippleButton/index.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { RippleContainer } from './styles'
+
+type RippleItem = {
+  x: number;
+  y: number;
+  size: number;
+}
+
+type RippleProps = {
+  duration: number;
+  color: string;
+}
+
+/**
+ * @param duration - Duration of a ripple effect.
+ * @param color - Background color of a ripple.
+ */
+
+export function Ripple({ duration = 850, color= '#ffffff' }: RippleProps) {
+  const [rippleArray, setRippleArray] = React.useState<RippleItem[] | []>([]);
+
+  React.useEffect(() => {
+    let bounce: number|undefined;
+
+    if (rippleArray.length > 0) {
+      window.clearTimeout(bounce);
+
+      bounce = window.setTimeout(() => {
+        setRippleArray([]);
+        window.clearTimeout(bounce);
+      }, duration * 4);
+    }
+
+    return () => window.clearTimeout(bounce);
+  }, [rippleArray.length, duration]);
+
+  const addRipple = (event: React.MouseEvent) => {
+    const rippleContainer = event.currentTarget.getBoundingClientRect();
+    const size =
+      rippleContainer.width > rippleContainer.height
+        ? rippleContainer.width
+        : rippleContainer.height;
+    const x = event.pageX - rippleContainer.x - size / 2;
+    const y = event.pageY - rippleContainer.y - size / 2;
+    const newRipple = {
+      x,
+      y,
+      size
+    };
+
+    setRippleArray([...rippleArray, newRipple]);
+  };
+
+  return (
+    <RippleContainer duration={duration} color={color} onMouseDown={addRipple}>
+      {rippleArray.length > 0 &&
+        (rippleArray as RippleItem[]).map((ripple, index) => {
+          return (
+            <span
+              key={"span" + index}
+              style={{
+                top: ripple.y,
+                left: ripple.x,
+                width: ripple.size,
+                height: ripple.size
+              }}
+            />
+          );
+        })}
+    </RippleContainer>
+  );
+}
